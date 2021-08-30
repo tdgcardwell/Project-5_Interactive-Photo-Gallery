ee2e9a88eccc2b6a0e496e20fb172dbc7dd2c662
Thomas Cardwell <Tom@ThomasCardwell.com>
2021-08-29 21:54:47 -0400

    mobile first grid layout, pseudo code for search function
    grid: 1 to 2 to 3 to 4 tiles

---
+++ js/mySearchBar.js
@@ -0,0 +1,18 @@
+// function
+  // get input from search bar value
+  // convert to lower case
+  // store as variable
+
+  // create ¿array? of the captions
+
+  // FOR each caption
+
+    // IF searchInput to lowercase is NOT included within caption
+      // set class of container to display none
+    // ELSE
+      // display as normal
+    //ENDIF
+
+  // ENDFOR
+
+// end function
